fd35ae5b4ca1868daee51f6278324fdfc883c31b
pavan <dongare.pavan25@gmail.com>
2020-05-02 06:29:13 +0530

    python complete

--- auto.py
+++ auto.py
@@ -5,19 +5,36 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+import time
+
 
 options = Options()
 options.add_argument("--user-data-dir=./check")
-driver = webdriver.Chrome(executable_path="./chromedriver", chrome_options=options)
-driver.get("https://web.whatsapp.com/send?phone=918698800448&text=I%27m+interested+in+your+car+for+sale&source&data&app_absent=true")
-#elements = driver.find_elements_by_class_name("._35EW6")
 
-delay = 6 # seconds
-try:
-    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME , '_35EW6')))
-    print ("Message Sent!")
-    driver.find_elements_by_class_name("_35EW6")[0].click()
-except TimeoutException:
-    print ("Loading took too much time!")
 
-#
+
+def send(contact,msg):
+
+    for no in contact:
+       driver = webdriver.Chrome(executable_path="./chromedriver", chrome_options=options)
+       driver.get('https://web.whatsapp.com/send?phone='+ str(no) +'&text='+ msg +'&source&data&app_absent=true')
+       delay = 10 # seconds
+       try:
+           myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME , '_35EW6')))
+           driver.find_elements_by_class_name("_35EW6")[0].click()
+           print ("Message Sent to ", no ,"\n")
+           time.sleep(3)
+           driver.close()
+       except TimeoutException:
+           print ("Loading took too much time!")
+
+
+contact = [
+    919049481501,
+    918526262648,
+    918698800448,
+    919168238924
+]
+
+send(contact,'testing multiple contact msgs');
+
